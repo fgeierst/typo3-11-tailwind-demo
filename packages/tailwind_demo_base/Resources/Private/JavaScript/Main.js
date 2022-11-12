527af6f79a7162f2d5391d953a895c68842f7bcc
Florian Geierstanger <florian.geierstanger@pluswerk.ag>
2022-11-12 15:28:46 +0100

    [TASK] Header with menu component

--- packages/tailwind_demo_base/Resources/Private/JavaScript/Main.js
+++ packages/tailwind_demo_base/Resources/Private/JavaScript/Main.js
@@ -3,5 +3,10 @@ import { createApp } from 'petite-vue'
 // Mount petite-vue 
 // on all elements with the `data-reactive` attribute 
 // <div data-reactive v-scope="{ count: 0 }"></div>
-createApp().mount('[data-reactive]')
+
+const reactiveElements = document.querySelectorAll('[data-reactive]');
+reactiveElements.forEach(element => {
+	createApp().mount(element)
+})
+
 
